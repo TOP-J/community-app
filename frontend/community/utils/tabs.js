7809991575d180ec4cbe53e2f0689a179d5499d6
TOP-J <101296608+TOP-J@users.noreply.github.com>
2025-08-18 08:17:31 +0100

    Delete frontend/community directory

--- frontend/community/utils/tabs.js
+++
@@ -1,9 +0,0 @@
-import { Home, Layers, Mail, Plus, Users } from 'lucide-react-native'
-
-export const tabRoutes = [
-    { name: "Feed", url: "/home", icon: Home },
-    { name: "Stack", url: "/stack", icon: Layers },
-    { name: "Add", url: "/add", icon: Plus },
-    { name: "Space", url: "/space", icon: Users },
-    { name: "Message", url: "/message", icon: Mail }
-];
